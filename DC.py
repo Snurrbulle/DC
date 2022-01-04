d4f2c49116288470f741a67115b5b6785869b877
Wilhelm Wiberg <w@w5.se>
2022-01-04 10:30:31 +0100

    Konami and a bug
    I fixed the Konami code! I also fixed a value type bug and made the text in game better.

--- DC.py
+++ DC.py
@@ -18,13 +18,18 @@ while True:
         time.sleep(10)
         print(" done.")
         add_item("wooden log", count=5)
-    elif action == "Enter my inventory.":
+    elif action == "Open my inventory.":
         print("Inventory: {}".format(inventory))
     elif action == "Quit game.":
         sys.exit()
     elif action == "Jump off a cliff.":
         health = health - 4
+        print("You lost 4 health points.")
     elif action == "Check my health.":
-        print("Your health is " + health + ".")
+        print("Your health is " + str(health) + ".")
+    elif action == "Up, up, down, down, left, right, left, right, b, a, start.":
+        add_item("wooden log", 99)
+        health = 99
+        print("You cheater! Okay, here. Have evrything you need like ever in this game.")
     else:
         print("There is no action called " + action + ", please check your spelling, grammar or maybe you can't do that in the game.")
