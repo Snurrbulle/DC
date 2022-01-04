887ae0af5eae199a696982926240e2e4a5cf8876
Wilhelm Wiberg <w@w5.se>
2022-01-04 18:21:41 +0100

    2.0.a.0 Points
    I made a simple points system to show how good you are at the game.

--- DC.py
+++ DC.py
@@ -5,6 +5,7 @@ import random
 inventory = {}
 health = 10
 safeness = 0
+points = 0
 
 def have_item(it):
     return inventory.get(it, 0) > 0
@@ -28,6 +29,7 @@ while True:
         time.sleep(10)
         print(" done. You got 5 wooden logs.")
         add_item("wooden log", count=5)
+        points = points + 5
     elif action == "Open my inventory.":
         print("Inventory: {}".format(inventory))
     elif action == "Quit game.":
@@ -51,6 +53,7 @@ while True:
         add_item("earth potion", 999)
         add_item("gold", 999)
         add_item("diamond", 999)
+        points = 999
         print("You cheater! Okay, here. Have everything you'll ever need in this game.")
     elif action == "Build a starter house.":
         if drop_item("wooden log", 10):
@@ -58,6 +61,7 @@ while True:
             time.sleep(10)
             print(" done. You got 3 points safer.")
             safeness = safeness + 3
+            points = points + 10
         else:
             print("You need 10 wooden logs to build this.")
     elif action == "Check my safeness.":
@@ -72,14 +76,17 @@ while True:
             add_item("dead cave monster")
             add_item("stone", random.randint(1, 6))
             health = health - damage
+            points = points + 7
         elif event in range(11, 16):
             print("You found some iron.")
             add_item("iron", random.randint(1, 3))
             add_item("stone", random.randint(1, 6))
+            points = points + 12
         elif event in range(15, 18):
             print("You found gold!")
             add_item("gold")
             add_item("stone", random.randint(1, 6))
+            points = points + 24
         elif event == 18:
             if drop_item("fire potion"):
                 print("You fell in lava, but luckily, you had a fire potion to survive.")
@@ -101,15 +108,18 @@ while True:
                 add_item("earth potion")
                 tresure = "earth potion"
             print("You found a " + treasure + ".")
+            points = points + 34
         elif event == 20:
             print("You found a diamond! How lucky!")
             add_item("diamond")
+            points = points + 35
     elif action == "Build a stone house.":
         if drop_item("wooden log", 5) and drop_item("stone", 10):
             print("Building. Please wait...", end="", flush=True)
             time.sleep(20)
             print(" done. You got 8 points safer.")
             safeness = safeness + 8
+            points = points + 9
         else:
             print("You need 5 wooden logs and 10 stones to build this.")
     elif action == "Build a stone wall.":
@@ -118,6 +128,7 @@ while True:
             time.sleep(15)
             print(" done. You got 15 points safer.")
             safeness = safeness + 15
+            points = points + 9
         else:
             print("You need 1 wooden log and 10 stones to build this.")
     elif action == "Build an iron wall.":
@@ -126,6 +137,7 @@ while True:
             time.sleep(20)
             print(" done. You got 42 points safer.")
             safeness = safeness + 42
+            points = points + 42
         else:
             print("You need 1 wooden log and 20 iron to build to build this.")
     elif action == "Build a gold wall.":
@@ -134,6 +146,7 @@ while True:
             time.sleep(20)
             print(" done. You got 112 points safer.")
             safeness = safeness + 112
+            points = points + 113
         else:
             print("You need 1 wooden log and 20 gold to build this.")
     elif action == "Build a diamond wall.":
@@ -142,6 +155,7 @@ while True:
             time.sleep(20)
             print(" done. You got 498 points safer.")
             safeness = safeness + 498
+            points = points + 500
         else:
             print("You need 1 wooden log and 20 diamonds to build this.")
     elif action == "Build a forge.":
@@ -150,6 +164,7 @@ while True:
             time.sleep(30)
             print(" done. You got 74 points safer.")
             safeness = safeness + 74
+            points = points + 42
         else:
             print("You need 5 wooden logs, 15 stones, and 25 iron to build this.")
     elif action == "Build a mansion.":
@@ -158,6 +173,7 @@ while True:
             time.sleep(150)
             print(" done. You got 1234 points safer.")
             safeness = safeness + 1234
+            points = points + 500
         else:
             print("You need 40 wooden logs, 30 stones, 50 iron, 69 gold, and 37 diamonds to build this.")
     elif action == "Build a bank.":
@@ -166,8 +182,11 @@ while True:
             time.sleep(100)
             print(" done. You got 763 points safer.")
             safeness = safeness + 763
+            points = points + 113
         else:
             print("You need 10 wooden logs, 50 stones, 36 iron, and 60 gold to build this.")
+    elif action == "Check my level.":
+        print("You have " + str(points) +  ".")
     
     else:
         print("There is no action called '" + action + "', please check your spelling, grammar or maybe you can't do that in this game.")
