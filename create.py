e23e204dfad9614fcb1fe03181f2c4285dfdc257
Wilhelm Wiberg <w@w5.se>
2022-12-17 10:49:37 +0100

    Random
    This is from now on a part of the game. Why? Because I say so.

--- create.py
+++ create.py
@@ -1,7 +1,17 @@
 from tkinter import *
+import tkMessageBox
+import tkFont
 
-window = Tk()
-window.title("DC")
-window.configure(width=500, height=300)
-window.configure(bg="black")
-window.mainloop()
+def window():
+    window = Tk()
+    window.title("DC")
+    window.configure(width=500, height=300)
+    window.configure(bg="black")
+    window.mainloop()
+
+def background(window):
+    image2 =Image.open('C:\\Users\\\\D\\titlepage\\front.gif')
+    image1 = ImageTk.PhotoImage(image2)
+    w = image1.width()
+    h = image1.height()
+    app.geometry('%dx%d+0+0' % (w,h))
